8f4a4425c7ee3a556f43009dd9847c05c03670f1
proxxy1 <gbadamosisulaiman1993@gmail.com>
2024-01-06 16:36:51 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -35,17 +36,12 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
 
-    @classmethod
-    def setUpClass(cls):
-        """Run once before all tests"""
-        #{ other lines of code here ... }
-        talisman.force_https = False
-        
     def setUp(self):
         """Runs before each test"""
         db.session.query(Account).delete()  # clean up the last tests
